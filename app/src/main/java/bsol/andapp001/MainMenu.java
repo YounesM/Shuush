dbc3e6c2b8270b3a62d7b005b7566cd22c93d5be
YounesM <younes.mgharfaoui@gmail.com>
2016-02-11 12:46:00 +0000

    Alpha animation on menu buttons

--- app/src/main/java/bsol/andapp001/MainMenu.java
+++ app/src/main/java/bsol/andapp001/MainMenu.java
@@ -2,6 +2,10 @@ package bsol.andapp001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        TranslateAnimation ta = new TranslateAnimation(0,0,300,0);
+        ta.setDuration(1500);
+        ta.setFillAfter(true);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+        alphaAnimation.setDuration(1500);
+        alphaAnimation.setFillAfter(true);
+
+        ImageView logo  = (ImageView)findViewById(R.id.logo);
+        LinearLayout menuButtons = (LinearLayout)findViewById(R.id.menu_buttons);
+        logo.startAnimation(ta);
+        menuButtons.startAnimation(alphaAnimation);
+
+
     }
 }
