4d39046fef56cacecdd7ec20a007dc3ecd41eaf5
YounesM <younes.mgharfaoui@gmail.com>
2016-02-13 12:04:56 +0000

    Created PreferenceActivity

---
+++ app/src/main/java/bsol/andapp001/SettingsActivity.java
@@ -0,0 +1,14 @@
+package bsol.andapp001;
+
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
